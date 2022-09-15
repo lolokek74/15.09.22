89662cb4be0296f0642905249ab426aab27a9bc1
is41-rodenkoav <1>
2022-09-15 13:55:42 +0500

    v 1.0.1 Login and Register

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RequestValidationLogin;
+use App\Http\Requests\RequestValidationRegister;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Представления шаблона авторизации
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function login()
+    {
+        return view('login');
+    }
+
+    /**
+     * Фактическая авторизация пользователя из POST запроса
+     * @param RequestValidationLogin $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function loginPost(RequestValidationLogin $request)
+    {
+        if(Auth::attempt($request->validated())){
+            $request->session()->regenerate();
+
+            return redirect()->route('welcome');
+        }
+        return back()->with(['errorSuccess' => 'Неверный логин или пароль']);
+    }
+
+    /**
+     * Представления шаблона регистрации
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function register()
+    {
+        return view('register');
+    }
+
+    /**
+     *Операция регистрации нового пользователя
+     * @param RequestValidationRegister $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function registerPost(RequestValidationRegister $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+
+        return back()->with(['success' => true]);
+    }
+}
---
+++ laravel/resources/views/register.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Страница регистрации</title>
+</head>
+<body>
+<form action="{{ route('register') }}" method="POST">
+    @if(session()->has('success'))
+        <h3>Операция регистрации успешно выполнена!</h3>
+    @endif
+    @csrf
+    <input type="email" name="email" placeholder="Ваша почта:"><br>
+    @error('email')<p>{{ $message }}</p>@enderror
+    <input type="text" name="name" placeholder="Ваше имя:"><br>
+    @error('name')<p>{{ $message }}</p>@enderror
+    <input type="password" name="password" placeholder="Ваш пароль:"><br>
+    @error('password')<p>{{ $message }}</p>@enderror
+    <input type="password" name="password_confirmation" placeholder="Ваш пароль повторно:"><br>
+    <input type="text" name="age" placeholder="Ваш возраст:"><br>
+    <input type="text" name="size" placeholder="Ваш размер:"><br>
+    <input type="text" name="color" placeholder="Ваш цвет:"><br>
+    <input type="submit" value="Регистрация">
+</form>
+</body>
+</html>
